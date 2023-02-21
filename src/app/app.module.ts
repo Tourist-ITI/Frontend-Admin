0463ed11fb6eb0dc5fd2bd23e648bc59d363d235
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-02-21 19:10:58 +0200

    Project Structure+dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminSignupComponent } from './Pages/admin-signup/admin-signup.component';
+import { AdminLoginComponent } from './Pages/admin-login/admin-login.component';
+import { AdminHomeComponent } from './Pages/admin-home/admin-home.component';
+import { AdminProfileComponent } from './Pages/admin-profile/admin-profile.component';
+import { AdminCreateTourComponent } from './Pages/admin-create-tour/admin-create-tour.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminSignupComponent,
+    AdminLoginComponent,
+    AdminHomeComponent,
+    AdminProfileComponent,
+    AdminCreateTourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
